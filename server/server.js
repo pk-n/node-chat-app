3e736fc87b90b23b1980c60a29272da11b880abb
kpavan <kpavan@smartzip.com>
2018-08-30 18:39:14 +0530

    add socket io

--- server/server.js
+++ server/server.js
@@ -1,12 +1,24 @@
 const path = require('path');
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io')
 
 const publicPath = path.join(__dirname, '../public');
 const app = express();
-
+const server = http.createServer(app);
 const port = process.env.PORT || 3000
+const io = socketIO(server);
+
 app.use(express.static(publicPath));
 
-app.listen(port, ()=>{
+server.listen(port, ()=>{
     console.log(`Listing at the port ${port}`);
 });
+
+io.on('connection', (socket)=>{
+    console.log('new user connected');
+
+    socket.on('disconnect', ()=>{
+        console.log('Client hangedup');
+    });
+})
